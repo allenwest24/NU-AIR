19531f71c104d42573310ee86a4cf9ef321ebe8e
Allen West <58156836+allenwest24@users.noreply.github.com>
2020-04-26 23:20:29 -0400

    Update main.py

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,15 +1,29 @@
 # Imports required for any PyQt app.
 from fbs_runtime.application_context.PyQt5 import ApplicationContext
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
 
 # Assorted basic imports.
 import sys
 import os
 
+class AppContext(ApplicationContext):
+
+    def run(self):
+        window = QMainWindow()
+        window.resize(500, 500)
+        window.setWindowTitle("NU-AIR: (NU And Improved Registration)")
+        label = QLabel("Welcome Screen:")
+        label.setAlignment(Qt.AlignCenter)
+        window.setCentralWidget(label)
+        window.show()
+        # TODO: Allen setup work
+        return self.app.exec_()
+
+
+
 if __name__ == '__main__':
-    appctxt = ApplicationContext()       # 1. Instantiate ApplicationContext
-    window = QMainWindow()
-    window.resize(250, 150)
-    window.show()
-    exit_code = appctxt.app.exec_()      # 2. Invoke appctxt.app.exec_()
+    app = QApplication(sys.argv)
+    appctxt = AppContext()       # 1. Instantiate ApplicationContext
+    exit_code = appctxt.run()    # 2. Invoke appctxt.app.exec_()
     sys.exit(exit_code)
