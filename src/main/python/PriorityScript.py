a47cde335b08a9c512a18c25e0b67aed0ba20713
Allen West <58156836+allenwest24@users.noreply.github.com>
2020-04-29 13:14:09 -0400

    Update PriorityScript.py

--- src/main/python/PriorityScript.py
+++ src/main/python/PriorityScript.py
@@ -1,10 +1,13 @@
+# PyQt5 imports
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 
+# Standard Python imports.
 import sys
 import os
 
+# Class that will handle the CRN priority registering version of the application.
 class PriorityScript:
     def __init__(self, username, password, CRNs):
         self.username = username
