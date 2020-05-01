ccb8c99c26cb7d139b891654fd6ab6f2bf7d9d40
Allen West <58156836+allenwest24@users.noreply.github.com>
2020-05-01 13:56:04 -0400

    Login is running but getting stuck by duo

--- src/main/python/PriorityScript.py
+++ src/main/python/PriorityScript.py
@@ -5,6 +5,10 @@ from PyQt5.QtGui import *
 
 # Selenium imports.
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 # Standard Python imports.
 import sys
@@ -30,7 +34,15 @@ class PriorityScript:
     # Drvier is initialized during run()
     def login(self, driver):
         driver.get("https://my.northeastern.edu/")
-        # TBD below.
-        #driver.find_element_by_id("email").send_keys("username")
-        #driver.find_element_by_id("pass").send_keys("password")
-        #iver.find_element_by_id("loginbutton").click()
+        go_to_login = driver.find_elements_by_xpath('//*[@id="portlet_com_liferay_journal_content_web_portlet_JournalContentPortlet_INSTANCE_GhAIpHlwoE3O"]/div/div/div/div[2]/div/div[2]/div/a')[0]
+        go_to_login.click()
+        # TODO: Allen - replace the placeholder input with the user-given stuff
+        username_box = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.ID,'username')))
+        username_box.send_keys("given_username")
+        password_box = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.ID,'password')))
+        password_box.send_keys("given_password")
+        login = driver.find_elements_by_xpath("/html/body/section/div/div[1]/div/form/div[3]/button")[0]
+        login.click()
+        # TODO: ALlen - fix this part that is getting fucked up by duo
+        send_push = driver.find_elements_by_xpath('//*[@id="auth_methods"]/fieldset/div[1]/button')[0]
+        send_push.click()
