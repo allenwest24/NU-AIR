1c96dc0505e5ade1ec8d85363c17be52975eadd7
Allen West <58156836+allenwest24@users.noreply.github.com>
2020-04-30 21:51:59 -0400

    Update PriorityScript.py

--- src/main/python/PriorityScript.py
+++ src/main/python/PriorityScript.py
@@ -3,6 +3,9 @@ from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 
+# Selenium imports.
+from selenium import webdriver
+
 # Standard Python imports.
 import sys
 import os
@@ -13,3 +16,17 @@ class PriorityScript:
         self.username = username
         self.password = password
         self.crns = CRNs
+
+    # Called from the UI
+    def run(self):
+        # Have to add a chromedriver.
+        driver = webdriver.Chrome()
+        print("getting stuck here")
+        #self.login(driver)
+
+    # Drvier is initialized during run()
+    def login(self, driver):
+        driver.get ("https://neuidmsso.neu.edu/idp/profile/SAML2/POST/SSO?execution=e1s2")
+        driver.find_element_by_id("email").send_keys("username")
+        driver.find_element_by_id("pass").send_keys("password")
+        iver.find_element_by_id("loginbutton").click()
