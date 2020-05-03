94d4579920580b23cb5f9450b2d3d2c5f2fbca57
Allen West <58156836+allenwest24@users.noreply.github.com>
2020-05-03 17:31:45 -0400

    Update PriorityScript.py

--- src/main/python/PriorityScript.py
+++ src/main/python/PriorityScript.py
@@ -13,6 +13,9 @@ from selenium.webdriver.common.by import By
 # Standard Python imports.
 import sys
 import os
+import time
+
+PASS = "password"
 
 # Class that will handle the CRN priority registering version of the application.
 class PriorityScript:
@@ -28,21 +31,39 @@ class PriorityScript:
         # TODO: Allen - figure out how to get_resource for the chromedriver.
         #chromedriver = self.get_resource('chromedriver')
         driver = webdriver.Chrome(chromedriver)
-        #driver.get("http:google.com")
         self.login(driver)
 
-    # Drvier is initialized during run()
+    # Driver is initialized during run()
     def login(self, driver):
         driver.get("https://my.northeastern.edu/")
         go_to_login = driver.find_elements_by_xpath('//*[@id="portlet_com_liferay_journal_content_web_portlet_JournalContentPortlet_INSTANCE_GhAIpHlwoE3O"]/div/div/div/div[2]/div/div[2]/div/a')[0]
         go_to_login.click()
         # TODO: Allen - replace the placeholder input with the user-given stuff
         username_box = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.ID,'username')))
-        username_box.send_keys("given_username")
+        username_box.send_keys("west.all")
         password_box = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.ID,'password')))
-        password_box.send_keys("given_password")
+        password_box.send_keys(PASS)
+        print("hello)")
         login = driver.find_elements_by_xpath("/html/body/section/div/div[1]/div/form/div[3]/button")[0]
         login.click()
-        # TODO: ALlen - fix this part that is getting fucked up by duo
-        send_push = driver.find_elements_by_xpath('//*[@id="auth_methods"]/fieldset/div[1]/button')[0]
+        WebDriverWait(webdriver, 5)
+        driver.switch_to.frame("duo_iframe")
+        send_push = driver.find_element_by_xpath('//*[@id="auth_methods"]/fieldset/div[1]/button')
         send_push.click()
+        WebDriverWait(driver, 10)
+        driver.switch_to.default_content()
+        WebDriverWait(driver, 10)
+        driver.execute_script("window.open('https://google.com','_blank')")
+        driver.switch_to.window(driver.window_handles[0])
+        time.sleep(10)
+        print("Got here1")
+        driver.switch_to.window(driver.window_handles[1])
+        driver.get("https://nubanner.neu.edu/StudentRegistrationSsb")
+
+    def link_has_gone_stale(self, driver):
+        try:
+            # poll the link with an arbitrary call
+            driver.find_elements_by_id('mail')
+            return False
+        except StaleElementReferenceException:
+            return True
